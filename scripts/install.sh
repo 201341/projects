de664edc559c2c9712b62843953dfbf597088fd1
swj <1186093704@qq.com>
2022-10-12 10:15:10 +0800

    Adopt install.sh
    Signed-off-by: swj <1186093704@qq.com>

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,18 +1,18 @@
 rm -rf ../mds0/sbin/
 rm -rf ../mds1/sbin/
 rm -rf ../mds2/sbin/
-cp -rf ../../curve/curvefs/devops/projects/mds/sbin/ ../mds0/
-cp -rf ../../curve/curvefs/devops/projects/mds/sbin/ ../mds1/
-cp -rf ../../curve/curvefs/devops/projects/mds/sbin/ ../mds2/
+cp -rf ../bin/ ../mds0/
+cp -rf ../bin/ ../mds1/
+cp -rf ../bin/ ../mds2/
 
 rm -rf ../metaserver0/sbin/
 rm -rf ../metaserver1/sbin/
 rm -rf ../metaserver2/sbin/
-cp -rf ../../curve/curvefs/devops/projects/metaserver/sbin/ ../metaserver0/
-cp -rf ../../curve/curvefs/devops/projects/metaserver/sbin/ ../metaserver1/
-cp -rf ../../curve/curvefs/devops/projects/metaserver/sbin/ ../metaserver2/
+cp -rf ../bin/ ../metaserver0/
+cp -rf ../bin/ ../metaserver1/
+cp -rf ../bin/ ../metaserver2/
 
 rm -rf ../tools/sbin/
 rm -rf ../client/sbin/
-cp -rf ../../curve/curvefs/devops/projects/tools/sbin/ ../tools/
-cp -rf ../../curve/curvefs/devops/projects/client/sbin/ ../client/
+cp -rf ../bin/ ../tools/
+cp -rf ../bin/ ../client/
